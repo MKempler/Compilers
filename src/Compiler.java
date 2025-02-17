f0c586a5ee72aba0418bb9a8309dccdc7f085e61
Max Kempler <98927804+MKempler@users.noreply.github.com>
2025-02-17 00:44:11 -0500

    More descriptive variable names and I tried to improve the readability of the code as well as improve comments to be more clear

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,6 +1,6 @@
 public class Compiler {
     private Lexer lexer;
-    private boolean verboseMode = true;  
+    private boolean verboseMode = true;  //always true for debugging
 
    
     public static void main(String[] args) {
@@ -15,83 +15,86 @@ public class Compiler {
     
     public void compile(String fileName) {
         try {
-            // Read the input file
-            String input = readFile(fileName);
+            // Read the programText file
+            String programText = readFile(fileName);
             
-            // Create lexer
-            lexer = new Lexer(input);
+            // Creates the lexer
+            lexer = new Lexer(programText);
             
             // Process tokens
-            processTokens();
+            processTokenStream();
             
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
     
-    // Process tokens from the lexer
-    private void processTokens() {
-        Token token;
+    // Process tokens and handles multiple programs seperated by $
+    private void processTokenStream() {
+        Token currentToken;
         int programCount = 0;
-        boolean FinalTokenWasEOP = false;
+        boolean foundEOP = false; // EOP = End of Program aka ($)
         int errorCount = 0;
         int warningCount = 0;
         
         System.out.println("INFO  Lexer - Lexing program " + (programCount + 1) + "...");
         
         do {
-            token = lexer.nextToken();
+            currentToken = lexer.nextToken();
             
             if (verboseMode) {
-                System.out.println("DEBUG Lexer - " + token);
+                System.out.println("DEBUG Lexer - " + currentToken);
             }
             
-            // Count all errors and warnings 
-            if (token.getType() == Token.Type.EOF && token.getLexeme().startsWith("Error")) {
+            // Count all errors and warnings for stats
+            if (currentToken.getType() == Token.Type.EOF && currentToken.getLexeme().startsWith("Error")) {
                 errorCount++;
-                // Store the error token but continue to the next token
+                // when an error is hit skip to the next token
                 continue;
-            } else if (token.getType() == Token.Type.WARNING) {
-                warningCount++;
+
+            } else if (currentToken.getType() == Token.Type.WARNING) {
+                warningCount++; // skip to next token after warning
             }
             
-            // Check for end of program marker
-            if (token.getType() == Token.Type.EOP) {
-                FinalTokenWasEOP = true;
+            // Check if the current token is the end of a program
+            if (currentToken.getType() == Token.Type.EOP) {
+                foundEOP = true;
                 System.out.println("INFO  Lexer - The Lexer finished with " + errorCount + " errors and " + warningCount + " warnings");
                 programCount++; 
                 Token nextToken = lexer.nextToken();
-                if (nextToken.getType() != Token.Type.EOF) {  // Only print if there's more input
+
+                if (nextToken.getType() != Token.Type.EOF) {  // there are more programs
                     System.out.println("INFO  Lexer - Lexing program " + (programCount + 1) + "...");
                 }
-                token = nextToken; 
-            } else if (token.getType() != Token.Type.EOF) {
-                FinalTokenWasEOP = false;
+                currentToken = nextToken; 
+
+            } else if (currentToken.getType() != Token.Type.EOF) {
+                foundEOP = false;
             }
             
-        } while (token.getType() != Token.Type.EOF || token.getLexeme().startsWith("Error"));
+        } while (currentToken.getType() != Token.Type.EOF || currentToken.getLexeme().startsWith("Error"));
         
         // Check for missing $ at end of program
-        if (!FinalTokenWasEOP && token.getType() == Token.Type.EOF) {
+        if (!foundEOP && currentToken.getType() == Token.Type.EOF) {
             System.out.println("WARNING: Missing $ at end of program " + programCount);
             warningCount++;
             System.out.println("INFO  Lexer - The Lexer finished with " + errorCount + " errors and " + warningCount + " warnings");
         }
         
         // final stats
-        System.out.println("\nFinal Stats! :");
+        System.out.println("\nFinal Stats!:");
         System.out.println("Total Programs: " + programCount);
         System.out.println("Total Errors: " + errorCount);
         System.out.println("Total Warnings: " + warningCount);
     }
     
-    // Read input file
+    // Reads the programText and returns it's contents as a string
     private String readFile(String fileName) throws Exception {
         try {
             return new String(java.nio.file.Files.readAllBytes(
                 java.nio.file.Paths.get(fileName)));
         } catch (Exception e) {
-            throw new Exception("Could not read input file: " + fileName);
+            throw new Exception("Could not read programText file: " + fileName);
         }
     }
     
