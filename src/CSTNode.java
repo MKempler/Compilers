7b0a8ddc24c7d1b276d0aef6217b0fd3a54a965f
Max Kempler <98927804+MKempler@users.noreply.github.com>
2025-03-16 18:29:45 -0400

    clear comments and some readaiblity fixes for cstnode.java

--- src/CSTNode.java
+++ src/CSTNode.java
@@ -1,55 +1,48 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
+// Node class for the Concrete Syntax Tree
 public class CSTNode {
     private String name;
     private Token token;
     private List<CSTNode> children;
     
-   
-      
-     
+    // Constructor for non terminals
     public CSTNode(String name) {
         this.name = name;
         this.token = null;
         this.children = new ArrayList<>();
     }
     
- 
+    // For terminals
     public CSTNode(String name, Token token) {
         this.name = name;
         this.token = token;
         this.children = new ArrayList<>();
     }
     
-  
     public void addChild(CSTNode child) {
-
         children.add(child);
     }
     
- 
     public List<CSTNode> getChildren() {
         return children;
     }
     
-   
     public String getName() {
         return name;
     }
     
-   
     public Token getToken() {
         return token;
     }
     
-   
+    // Terminal nodes have tokens
     public boolean isTerminal() {
         return token != null;
     }
     
-
+    // show the CST from this node down
     public void display() {
         display(0);
     }
@@ -65,7 +58,7 @@ public class CSTNode {
             
             System.out.println(indent + "[" + name + "]");
         } else {
-            
+           
             System.out.println(indent + "<" + name + ">");
             
             for (CSTNode child : children) {
