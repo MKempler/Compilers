9591aef317ae69a3241114de051009a74c1761d4
Max Kempler <98927804+MKempler@users.noreply.github.com>
2025-04-13 15:15:30 -0400

    added method to handle print statements for the AST conversion

--- src/ASTBuilder.java
+++ src/ASTBuilder.java
@@ -4,6 +4,10 @@ public class ASTBuilder {
     public ASTBuilder() {
     }
     
+    public ASTBuilder(boolean verboseMode) {
+        this.verboseMode = verboseMode;
+    }
+    
     public ASTNode buildAST(CSTNode cstRoot) {
         if (verboseMode) {
             System.out.println("AST Builder: Building AST from CST");
@@ -61,6 +65,61 @@ public class ASTBuilder {
             
             if (verboseMode) {
                 System.out.println("AST Builder: Statement type is " + childName);
+            }
+            
+            if (childName.equals("Print Statement")) {
+                convertPrintStatement(statementChild, parentNode);
+            }
+        }
+    }
+
+    private void convertPrintStatement(CSTNode printNode, ASTNode parentNode) {
+        if (verboseMode) {
+            System.out.println("AST Builder: Converting Print Statement");
+        }
+        
+        int line = 0;
+        int column = 0;
+        
+        if (printNode.getChildren().size() >= 1) {
+            Token printToken = printNode.getChildren().get(0).getToken();
+            
+            if (printToken != null) {
+                line = printToken.getLine();
+                column = printToken.getColumn();
+            }
+        }
+        
+        ASTNode printASTNode = new ASTNode("Print_Statement", line, column);
+        parentNode.addChild(printASTNode);
+        
+        if (printNode.getChildren().size() >= 3) {
+            CSTNode exprNode = printNode.getChildren().get(2);
+            convertExpression(exprNode, printASTNode);
+        }
+    }
+
+    private void convertExpression(CSTNode exprNode, ASTNode parentNode) {
+        if (verboseMode) {
+            System.out.println("AST Builder: Converting Expression");
+        }
+        
+        if (!exprNode.getChildren().isEmpty()) {
+            CSTNode exprChild = exprNode.getChildren().get(0);
+            
+            if (exprChild.isTerminal()) {
+                String value = exprChild.getToken().getLexeme();
+                int line = exprChild.getToken().getLine();
+                int column = exprChild.getToken().getColumn();
+                
+                ASTNode valueNode = new ASTNode("Value", value, line, column);
+                parentNode.addChild(valueNode);
+            
+            } else {
+                ASTNode valueNode = new ASTNode("Expression", 
+                                               exprChild.getToken() != null ? exprChild.getToken().getLine() : 0,
+                                               exprChild.getToken() != null ? exprChild.getToken().getColumn() : 0);
+                parentNode.addChild(valueNode);
             }
         }
     }
