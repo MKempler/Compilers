2a4a1aac764b3645e2d7fb6a6af1594010139479
Max Kempler <98927804+MKempler@users.noreply.github.com>
2025-04-12 20:03:11 -0400

    process statements inside blocks

--- src/ASTBuilder.java
+++ src/ASTBuilder.java
@@ -30,6 +30,38 @@ public class ASTBuilder {
                                           blockNode.getToken() != null ? blockNode.getToken().getLine() : 0,
                                           blockNode.getToken() != null ? blockNode.getToken().getColumn() : 0);
         
+        if (blockNode.getChildren().size() > 1) {
+            CSTNode statementListNode = blockNode.getChildren().get(1);
+            convertStatementList(statementListNode, blockASTNode);
+        }
+        
         return blockASTNode;
+    }
+
+    private void convertStatementList(CSTNode statementListNode, ASTNode parentNode) {
+        if (verboseMode) {
+            System.out.println("AST Builder: Converting Statement List");
+        }
+        
+        for (CSTNode child : statementListNode.getChildren()) {
+            if (child.getName().equals("Statement")) {
+                convertStatement(child, parentNode);
+            }
+        }
+    }
+
+    private void convertStatement(CSTNode statementNode, ASTNode parentNode) {
+        if (verboseMode) {
+            System.out.println("AST Builder: Converting Statement");
+        }
+        
+        if (!statementNode.getChildren().isEmpty()) {
+            CSTNode statementChild = statementNode.getChildren().get(0);
+            String childName = statementChild.getName();
+            
+            if (verboseMode) {
+                System.out.println("AST Builder: Statement type is " + childName);
+            }
+        }
     }
 } 
