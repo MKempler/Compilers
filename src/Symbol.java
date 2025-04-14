2043b7f877d2bb0445f7e27c00edfa324471a0d7
Max Kempler <98927804+MKempler@users.noreply.github.com>
2025-04-13 22:02:51 -0400

    I created tje basic structrue for representing the variables in the symbol table

---
+++ src/Symbol.java
@@ -0,0 +1,59 @@
+public class Symbol {
+    private String name;        
+    private String type;       
+    private int scope;      
+    private int line;   
+    private int column;  
+    private boolean isInitialized = false; 
+    private boolean isUsed = false;     
+
+    public Symbol(String name, String type, int scope, int line, int column) {
+        this.name = name;
+        this.type = type;
+        this.scope = scope;
+        this.line = line;
+        this.column = column;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+
+    public String getType() {
+        return type;
+    }
+    
+    public int getScope() {
+        return scope;
+    }
+    
+    public int getLine() {
+        return line;
+    }
+    
+    public int getColumn() {
+        return column;
+    }
+    
+
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+    
+
+    public void setInitialized(boolean initialized) {
+        isInitialized = initialized;
+    }
+    
+    public boolean isUsed() {
+        return isUsed;
+    }
+  
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
+    public String toString() {
+        return name + "\t" + type + "\t" + scope + "\t" + line;
+    }
+} 
