859d175bccd0f7cf8c62caa48cd39f380eeb7e57
Max Kempler <98927804+MKempler@users.noreply.github.com>
2025-04-13 23:16:51 -0400

    funcitonality to add symbols to symbol table

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class SymbolTable {
@@ -20,7 +21,32 @@ public class SymbolTable {
             currentScope--;
         }
     }
+    
     public int getCurrentScope() {
         return currentScope;
+    }
+
+    public boolean add(Symbol symbol) {
+        // Check to see if the symbol already exists in the current scope
+        List<Symbol> existingSymbols = table.get(symbol.getName());
+        if (existingSymbols != null) {
+            for (Symbol existing : existingSymbols) {
+                if (existing.getScope() == symbol.getScope()) {
+                    
+                    return false;
+                }
+            }
+        }
+        
+        // Get the list for this name
+        List<Symbol> symbols = table.get(symbol.getName());
+        if (symbols == null) {
+            symbols = new ArrayList<>();
+            table.put(symbol.getName(), symbols);
+        }
+        
+        // Add the symbol
+        symbols.add(symbol);
+        return true;
     }
 } 
