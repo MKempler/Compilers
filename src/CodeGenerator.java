09abf17bc453f4c25fc337f025d842717f847975
Max Kempler <98927804+MKempler@users.noreply.github.com>
2025-04-30 15:39:45 -0400

    core recursive structure for traversing the AST for code gen

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -17,7 +17,8 @@ public class CodeGenerator {
     private static final String CPX = "EC";        
     private static final String BNE = "D0";         
     private static final String INC = "EE";        
-    private static final String SYS = "FF";       
+    private static final String SYS = "FF";
+    private static final String BRK = "00";
     
     
     private static final int MEMORY_START = 0x0010;
@@ -41,16 +42,82 @@ public class CodeGenerator {
         // Clear previous code
         machineCode.setLength(0);
         
-     
+        
         appendComment("6502a Machine Code - Generated from source");
         appendComment("Memory allocation starts at " + toHexString(MEMORY_START));
         
+        generateCode(ast);
         
         append(BRK, "BRK", "Program End");
         
         return machineCode.toString();
     }
     
+    private void generateCode(ASTNode node) {
+        if (node == null) {
+            return;
+        }
+        
+        if (verboseMode) {
+            System.out.println("CODE GENERATOR: Processing node of type " + node.getType());
+        }
+        
+        // Process node based on its type
+        switch (node.getType()) {
+            case "BLOCK":
+                generateBlockCode(node);
+                break;
+            
+                case "Print_Statement":
+                generatePrintCode(node);
+                break;
+            
+                case "Variable_Declaration":
+                generateVarDeclCode(node);
+                break;
+            
+                case "Assignment_Statement":
+                generateAssignmentCode(node);
+                break;
+            
+                case "If_Statement":
+                generateIfCode(node);
+                break;
+            
+                case "While_Statement":
+                generateWhileCode(node);
+                break;
+            
+                default:
+                break;
+        }
+    }
+    
+    private void generateBlockCode(ASTNode node) {
+        appendComment("Block Start");
+        
+        // Process each child node in the block
+        for (ASTNode child : node.getChildren()) {
+            generateCode(child);
+        }
+        
+        appendComment("Block End");
+    }
+    
+    private void generatePrintCode(ASTNode node) {
+    }
+    
+    private void generateVarDeclCode(ASTNode node) {
+    }
+    
+    private void generateAssignmentCode(ASTNode node) {
+    }
+    
+    private void generateIfCode(ASTNode node) {
+    }
+    
+    private void generateWhileCode(ASTNode node) {
+    }
    
     private void append(String opcode, String mnemonic, String comment) {
         machineCode.append(opcode).append(" ; ").append(mnemonic);
@@ -60,7 +127,7 @@ public class CodeGenerator {
         machineCode.append("\n");
     }
     
-
+ 
     private void appendComment(String comment) {
         machineCode.append("; ").append(comment).append("\n");
     }
